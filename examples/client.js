b7c283f54c9cad12aab381e61498e2604c17e848
prdn <paolo.ardoino@gmail.com>
2017-01-30 15:08:06 +0000

    mc

--- examples/client.js
+++ examples/client.js
@@ -18,7 +18,7 @@ setTimeout(() => {
   var d1 = new Date()
   for (var i = 0; i < reqs; i++) {
     client.request('test', 'hello', { timeout: 10000 }, (err, data) => {
-      //console.log(err, data)
+      console.log(err, data)
       if (++reps === reqs) {
         var d2 = new Date()
         console.log(d2 - d1) 
