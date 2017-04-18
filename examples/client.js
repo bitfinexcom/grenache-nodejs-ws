75d8eb6ca58bdf801e051e395ce59949835bfae6
prdn <paolo.ardoino@gmail.com>
2017-04-18 21:58:23 +0000

    package bump

--- examples/client.js
+++ examples/client.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var fs = require('fs')
 var _ = require('lodash')
 var Base = require('grenache-nodejs-base')
 var Peer = require('./../lib/Peer')
@@ -11,17 +12,27 @@ link.start()
 
 var client = new Peer(link, {})
 
-var reqs = 5000
+var reqs = 100
 var reps = 0
 
+
+const elapsed_time = function(start, note) {
+  var precision = 3; // 3 decimal places
+  var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+  console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+  start = process.hrtime(); // reset the timer
+}
+
+var big = fs.readFileSync('/home/dev/data/arvicco.txt')
+
 setTimeout(() => {
-  var d1 = new Date()
+  let start = process.hrtime()
   for (var i = 0; i < reqs; i++) {
-    client.request('test', 'hello', { timeout: 10000 }, (err, data) => {
+    client.request('test', JSON.stringify(big), { timeout: 10000 }, (err, data) => {
       //console.log(err, data)
       if (++reps === reqs) {
         var d2 = new Date()
-        console.log(d2 - d1) 
+        elapsed_time(start, 'finished')
       }
     })
   }
