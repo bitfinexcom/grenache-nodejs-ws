f5259ea847fc0111223e1f894b80b56851da40af
prdn <paolo.ardoino@gmail.com>
2017-04-21 14:07:19 +0000

    refactoring

--- examples/client.js
+++ examples/client.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const _ = require('lodash')
 const Base = require('grenache-nodejs-base')
-const Peer = require('./../lib/Peer')
+const Peer = require('./../lib/PeerRPC')
 
 const link = new Base.Link({
   grape: 'ws://127.0.0.1:30002'
@@ -12,9 +12,8 @@ link.start()
 
 const client = new Peer(link, {})
 
-const reqs = 10000
+const reqs = 10
 let reps = 0
-
 
 const elapsed_time = function(start, note) {
   const precision = 3; // 3 decimal places
@@ -27,7 +26,7 @@ setTimeout(() => {
   let start = process.hrtime()
   for (let i = 0; i < reqs; i++) {
     client.request('test', 'here', { timeout: 10000 }, (err, data) => {
-      console.log(err, data)
+      //console.log(err, data)
       if (++reps === reqs) {
         elapsed_time(start, 'finished')
       }
--- examples/worker.js
+++ examples/worker.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash')
 const Base = require('grenache-nodejs-base')
-const Peer = require('./../lib/Peer')
+const Peer = require('./../lib/PeerRPC')
 
 const link = new Base.Link({
   grape: 'ws://127.0.0.1:30001'
@@ -10,11 +10,12 @@ const link = new Base.Link({
 link.start()
 
 const worker = new Peer(link, {})
-const service = worker.listen('req', 5000)
+const service = worker.transport('server')
+service.listen(50000)
 
 setInterval(function() {
   worker.announce('test', service.port, {}, () => {
-    console.log('announced')
+    console.log('announced', service.port)
   })
 
   const v = 'hello'
@@ -24,7 +25,6 @@ setInterval(function() {
   })
 }, 1000)
 
-worker.on('request', (rid, key, payload, handler) => {
-  //console.log('worker', rid, key, payload)
-  handler.reply('world')
+service.on('request', (rid, key, payload, handler) => {
+//  handler.reply('world')
 })
