35bce8f3042df3675cda5941f3bf93f88b122419
prdn <paolo.ardoino@gmail.com>
2017-05-12 17:52:30 +0000

    put_get example

---
+++ examples/put_get.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const _ = require('lodash')
+const Base = require('grenache-nodejs-base')
+const Peer = require('./../lib/PeerRPC')
+
+const link = new Base.Link({
+  grape: 'ws://127.0.0.1:30001'
+})
+link.start()
+  
+setInterval(() => {
+  link.put({ v: 'hello world' }, (err, hash) => {
+    console.log('data saved to the DHT', err, hash)
+    if (hash) {
+      link.get(hash, (err, res) => {
+        console.log('data requested to the DHT', err, res)
+      })
+    }
+  })
+}, 2000)
