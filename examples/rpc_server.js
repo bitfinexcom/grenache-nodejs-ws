1cc761e0e1db21634136f0fbb6d277b7218f92ee
prdn <paolo.ardoino@gmail.com>
2017-05-14 12:19:36 +0000

    refactoring

---
+++ examples/rpc_server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Base = require('grenache-nodejs-base')
+const Peer = require('./../lib/PeerRPCServer')
+const _ = require('lodash')
+
+const link = new Base.Link({
+  grape: 'ws://127.0.0.1:30001'
+})
+link.start()
+
+const peer = new Peer(link, {})
+peer.init()
+
+const service = peer.transport('server')
+service.listen(_.random(1000) + 1024)
+
+setInterval(function() {
+  link.announce('test', service.port, {})
+}, 1000)
+
+service.on('request', (rid, key, payload, handler) => {
+  //console.log('peer', rid, key, payload)
+  handler.reply('world')
+})
