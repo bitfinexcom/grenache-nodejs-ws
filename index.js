e712fea74cf2f5174cbd57b65cf087d1b195d658
prdn <paolo.ardoino@gmail.com>
2017-05-15 19:56:36 +0000

    package bump

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-module.exports.PeerRPC = require('./lib/PeerRPC')
+module.exports.PeerRPCClient = require('./lib/PeerRPCClient')
+module.exports.PeerRPCServer = require('./lib/PeerRPCServer')
+module.exports.PeerSub = require('./lib/PeerSub')
+module.exports.PeerPub = require('./lib/PeerPub')
