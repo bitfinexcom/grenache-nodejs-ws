9a6a17b3c6e054ae74c8ebfd874dbc4d55a9494e
prdn <paolo.ardoino@gmail.com>
2017-05-18 19:49:58 +0000

    rpc client map test

---
+++ examples/rpc_client_map.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Base = require('grenache-nodejs-base')
+const Peer = require('./../lib/PeerRPCClient')
+
+const link = new Base.Link({
+  grape: 'ws://127.0.0.1:30001'
+})
+link.start()
+
+const peer = new Peer(link, {})
+peer.init()
+
+const reqs = 10
+let reps = 0
+
+setTimeout(() => {
+  for (let i = 0; i < reqs; i++) {
+    peer.map('rpc_test', 'hello', { timeout: 10000 }, (err, data) => {
+      console.log(err, data)
+    })
+  }
+}, 2000)
