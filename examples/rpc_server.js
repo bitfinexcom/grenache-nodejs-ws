8a3f7736131edbce22e825a7695e518fb74efb74
Robert Kowalski <rok@kowalski.gd>
2017-05-22 15:13:34 +0200

    examples: add usage instructions

--- examples/rpc_server.js
+++ examples/rpc_server.js
@@ -1,3 +1,6 @@
+// make sure you start grenache-grape:
+// grape --dp 20001 --apw 30001 --aph 40001 --bn "127.0.0.1:20002,127.0.0.1:20003"
+
 'use strict'
 
 const Base = require('grenache-nodejs-base')
